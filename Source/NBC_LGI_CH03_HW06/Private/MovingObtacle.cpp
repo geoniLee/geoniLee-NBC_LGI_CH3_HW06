eb196715737028e6bf7a1d356a2cf57e12bcfc5d
geoni-ara <lgeeckh@gmail.com>
2026-04-10 18:03:34 +0900

    움직이는 발판 구현

---
+++ Source/NBC_LGI_CH03_HW06/Private/MovingObtacle.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingObtacle.h"
+
+// Sets default values
+AMovingObtacle::AMovingObtacle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Game/ModularOldTown/Meshes/CliffsRocks/SM_Rock_03.SM_Rock_03"));
+	if (MeshAsset.Succeeded()) {
+		StaticMeshComp->SetStaticMesh(MeshAsset.Object);
+	}
+
+	SetActorScale3D(FVector(4));
+	StartPosition = FVector(1000, 2000, 0);
+	MaxRange = 500;
+	MoveSpeed = 100;
+	Direction = -1;
+}
+
+// Called when the game starts or when spawned
+void AMovingObtacle::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetActorLocation(StartPosition);
+	
+}
+
+// Called every frame
+void AMovingObtacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AddActorWorldOffset(FVector(0, MoveSpeed * Direction, 0) * DeltaTime);
+	double CurrentY = GetActorLocation().Y;
+	if (Direction == -1 && CurrentY < (StartPosition.Y - MaxRange)) Direction = 1;
+	else if (Direction == 1 && CurrentY > (StartPosition.Y + MaxRange)) Direction = -1;
+}
---
+++ Source/NBC_LGI_CH03_HW06/Public/MovingObtacle.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingObtacle.generated.h"
+
+UCLASS()
+class NBC_LGI_CH03_HW06_API AMovingObtacle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingObtacle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	USceneComponent* SceneRoot;
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform|Obtacle")
+	float RotationSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform|Obtacle")
+	float MoveSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform|Obtacle")
+	float MaxRange;
+	
+	int32 Direction;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Platform|Obtacle")
+	FVector StartPosition;
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
